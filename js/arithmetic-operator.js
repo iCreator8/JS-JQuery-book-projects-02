d87fc97e57103e4e5e166de090d3c260a1c10175
iCreator-Git <iskandarjon8008@gmail.com>
2026-01-27 08:04:27 -0500

    c02-atithmetic-operator

--- js/arithmetic-operator.js
+++ js/arithmetic-operator.js
@@ -1,11 +1,11 @@
 // Create a variable for the subtotal and make a calculation
-var subtotal = (13 + 1) * 5; // Subtotal is 70
+var subtotal = (15 + 1) * 5; // Subtotal is 70
 
 // Create a variable for the shipping and make a calculation
-var shipping = 0.5 * (13 + 1); // Shipping is 7
+var shipping = 0.5 * (15 + 1); // Shipping is 7
 
 // Create the total by combining the subtotal and shipping values
-var total = subtotal + shipping; // Total is 77
+var total = subtotal + shipping + ' Our Best Deal'; // Total is 77
 
 // Write the results to the screen
 var elSub = document.getElementById('subtotal');
